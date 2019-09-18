4752bbc59e852017d3bdc4445d02563837c87903
Samer Shawar <samer.shawar@gmail.com>
2019-09-19 00:30:22 +0300

    First Commit
    before finalizing the whole scenes

---
+++ Test_01/Assets/Scripts/PanelCloser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelCloser : MonoBehaviour
+{
+  public GameObject Panel;
+
+public void OpenPanel()
+{
+
+    if (Panel != null)
+    {
+
+      //bool isActive = Panel.activeSelf;
+       Panel.SetActive(false);
+      //Panel.SetActive(!isActive);
+
+    }
+
+
+}
+}
---
+++ Test_01/Assets/Scripts/Panelscript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panelscript : MonoBehaviour
+{
+  public GameObject Panel1;
+  public GameObject Panel2;
+
+  public void OpenPanel()
+  {
+
+      if (Panel1 != null)
+      {
+
+         Panel1.SetActive(true);
+
+      }
+
+
+  }
+
+  public void ClosePanel()
+  {
+
+      if (Panel1 != null)
+      {
+
+
+         Panel1.SetActive(false);
+
+
+      }
+
+
+  }
+
+  public void OpenPanel2()
+  {
+
+      if (Panel2 != null)
+      {
+
+        //bool isActive = Panel.activeSelf;
+         Panel2.SetActive(true);
+        //Panel.SetActive(!isActive);
+
+      }
+
+
+  }
+
+  public void ClosePanel2()
+  {
+
+      if (Panel2 != null)
+      {
+
+
+         Panel2.SetActive(false);
+
+
+      }
+
+
+  }
+
+}
